94da40563b4b7cf55a6e17d5f660404d7b9a6c97
davejwelch <davejwelch@gmail.com>
2025-07-18 17:43:13 -0400

    Initial commit

---
+++ index.js
@@ -0,0 +1,14 @@
+const express = require('express');
+const cors = require('cors');
+
+const app = express();
+app.use(cors());
+
+app.get('/ping', (req, res) => {
+  res.send('pong');
+});
+
+const PORT = process.env.PORT || 3000;
+app.listen(PORT, () => {
+  console.log(`Server running on port ${PORT}`);
+});
